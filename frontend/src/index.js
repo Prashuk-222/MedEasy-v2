f90f83cf8637fabb958fca83e24823fae0110956
ayush singh <ayushsinghbharti123@gmail.com>
2024-12-19 03:50:29 +0530

    Basic login page setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomePage from "../src/app/HomePage.tsx"
+// import HomePage from "../src/app/HomePage.tsx"
+import Login from "../src/app/(auth)/Login.tsx"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    {/* <HomePage /> */}
+    <Login />
   </React.StrictMode>
 );
 
