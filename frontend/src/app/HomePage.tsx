026f20cb25eb82cd4e39f1a71fb12a322e8a32a6
ayush singh <ayushsinghbharti123@gmail.com>
2024-12-19 03:05:39 +0530

    Make Screen responsive

--- frontend/src/app/HomePage.tsx
+++ frontend/src/app/HomePage.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import img from "../assets/medical-stethoscope-white.jpg";
+import "./homepage.css";
 
 export default function HomePage() {
   return (
     <div
-    className="h-screen bg-[#DBDEE3]"
+      className="h-screen bg-[#DBDEE3] body"
       style={{
         backgroundImage: `url(${img})`,
-        backgroundSize: 'contain',
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: 'left',
       }}
     >
       {/* Header */}
       <div
-        className="flex justify-end gap-16 py-4 bg-transparent"
+        className="header flex justify-end gap-16 py-4 bg-transparent"
         style={{ right: "15%", position: "relative" }}
       >
         <a href="/" className="text-xl font-bold">
@@ -29,15 +27,10 @@ export default function HomePage() {
       </div>
 
       {/* Main Content */}
-      <div style={{ height: "70%" }} className="w-full justify-items-end">
-        <div
-          style={{
-            width: "30%",
-          }}
-          className="h-full mx-20 justify-items-center"
-        >
-          <div className="h-[50%] flex items-end justify-center">
-            <p className="text-2xl text-right">
+      <div className="mainBody w-full justify-items-end">
+        <div className="h-full mx-20 justify-items-center mainBodyIn">
+          <div className="h-[50%] flex items-end justify-center mainBodyTextContainer">
+            <p className="text-2xl text-right mainBodyText">
               USE AI FOR <br />
               <span className="text-3xl font-bold"> MORE DEATILED </span>
               AND <br /> <span className="text-3xl font-bold">ACCURATE</span>
@@ -61,31 +54,25 @@ export default function HomePage() {
       </div>
 
       {/* Footer */}
-      <div className="flex justify-between items-end">
-        <div className="flex gap-8">
-          <p
-            style={{ borderRadius: 8 }}
-            className="flex items-center mx-8 text-lg text-left h-12 px-12 bg-[#1c2932] text-white"
-          >
+      <div className="flex justify-between items-end footer">
+        <div className="flex gap-8 footer-btn-holder">
+          <p style={{ borderRadius: 8 }} className="footer-btns ml-8">
             How to use?
           </p>
-          <p
-            style={{ borderRadius: 8 }}
-            className="flex items-center text-lg text-right h-12 px-12 bg-[#1c2932] text-white"
-          >
+          <p style={{ borderRadius: 8 }} className="footer-btns">
             Learn more
           </p>
         </div>
 
         <p>
           <div>
-            <p
-              className="text-2xl font-semibold"
-              style={{ right: "35%", position: "relative" }}
-            >
-              <p className="text-7xl font-bold" style={{ color: "#1c2932" }}>
+            <p className="text-2xl font-semibold footer-text">
+              <p
+                className="text-5xl font-bold footer-text-main"
+                style={{ color: "#1c2932" }}
+              >
                 MEDEASY
-              </p>{" "}
+              </p>
               AI HEALTH ASSISTANT
             </p>
           </div>
